45c2c433da02467951eaa01cc7f5e804f70fa2b1
nyxnyx <szostak.grzegorz@gmail.com>
2019-10-17 13:04:51 +0200

    Added schedule to update location every 15 minutes

--- device_tracker.py
+++ device_tracker.py
@@ -2,6 +2,7 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 from homeassistant.util import slugify
+from homeassistant.helpers.event import track_utc_time_change
 
 from . import DOMAIN
 
@@ -10,7 +11,7 @@ def setup_scanner(hass, config, see, discovery_info=None):
     data = hass.data[DOMAIN]
     tracker = OnstarDeviceTracker(see, data)
     _LOGGER.info("onstar device_tracker set-up")
-    tracker.update()
+    tracker.setup(hass)
     return True
 
 class OnstarDeviceTracker:
@@ -20,6 +21,14 @@ class OnstarDeviceTracker:
         """Initialize the Tracker."""
         self._see = see
         self._data = data
+
+    def setup(self, hass):
+        """Set up a timer and start gathering devices."""
+        self.update()
+        track_utc_time_change(
+            hass, lambda now: self.update(), minute=range(0, 60, 15)
+        )
+
 
     def update(self) -> None:
         """Update the device info.
